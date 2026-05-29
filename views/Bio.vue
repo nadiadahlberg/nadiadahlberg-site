36ec052e5443d88efcd09dd9f41ca799c5294e6f
Nadia Dahlberg <nadiadahlberg@outlook.com>
2026-05-29 14:47:12 +0200

    Refactor education details in Bio.vue
    Updated the education section to improve clarity and formatting.

--- views/Bio.vue
+++ views/Bio.vue
@@ -77,10 +77,8 @@
         <h4 class="text-lg font-semibold">Bachelor’s Degree in Media and Communication Science</h4>
         <p class="text-gray-600 text-xs">Jönköping University • 2014 – 2017</p>
         <ul class="list-disc pl-5 mt-2 space-y-1 text-sm font-thin">
-            <li>Specialization: Marketing Communication and PR</li>
-        <p class="text-gray-600 text-xs">University of Canberra • Australia</p>
-        <ul class="list-disc pl-5 mt-2 space-y-1 text-sm font-thin">
-            <li>Exchange semester</li>
+            <p class="font-thin text-sm pb-2.5">Specialization: Marketing Communication and PR</p>
+            <li>Exchange semester: University of Canberra, Australia</li>
         <p class="font-thin text-sm pb-2.5">Studied couses in:</p>
             <ul class="list-disc pl-5 mt-2 space-y-1 text-sm font-thin">
             <li>- Course name 1</li>
@@ -90,7 +88,6 @@
                 </ul>
         </ul>
     </section>
-
     
     <section class="pt-6">
         <h4 class="text-lg font-semibold">Game Programming</h4>
