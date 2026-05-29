71f24a052a3c2e953c89fa785de49f693266d543
Nadia Dahlberg <nadiadahlberg@outlook.com>
2026-05-29 11:48:47 +0200

    Update course names in Bio.vue

--- views/Bio.vue
+++ views/Bio.vue
@@ -44,12 +44,12 @@
         <ul class="list-disc pl-5 mt-2 space-y-1 text-sm font-thin">
             <li>Specialization: Marketing Communication and PR</li>
             <li>Exchange semester: University of Canberra, Australia</li>
-            <ul class="list-disc pl-5 mt-2 space-y-1 text-sm font-thin">
             <li>Studied courses in:</li>
-            <li>- [Course name 1]</li>
-            <li>- [Course name 2]</li>
-            <li>- [Course name 3]</li>
-            <li>- [Course name 4]</li>
+            <ul class="list-disc pl-5 mt-2 space-y-1 text-sm font-thin">
+            <li>- Course name 1</li>
+            <li>- Course name 2</li>
+            <li>- Course name 3</li>
+            <li>- Course name 4</li>
                 </ul>
         </ul>
     </section>
