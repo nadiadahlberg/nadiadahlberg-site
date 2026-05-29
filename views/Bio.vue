bdb51a08976d24eda90df5d4aae4da534bb3ad88
Nadia Dahlberg <nadiadahlberg@outlook.com>
2026-05-29 14:34:04 +0200

    Change specialization list item to paragraph format

--- views/Bio.vue
+++ views/Bio.vue
@@ -77,7 +77,7 @@
         <h4 class="text-lg font-semibold">Bachelor’s Degree in Media and Communication Science</h4>
         <p class="text-gray-600 text-xs">Jönköping University • 2014 – 2017</p>
         <ul class="list-disc pl-5 mt-2 space-y-1 text-sm font-thin">
-            <li>Specialization: Marketing Communication and PR</li>
+            <p class="font-thin text-sm pb-2.5">Specialization: Marketing Communication and PR</p>
             <li>Exchange semester: University of Canberra, Australia</li>
         <p class="font-thin text-sm pb-2.5">Studied couses in:</p>
             <ul class="list-disc pl-5 mt-2 space-y-1 text-sm font-thin">
