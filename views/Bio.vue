c84943b281ac92465611e47c60c24387c14e493e
Nadia Dahlberg <nadiadahlberg@outlook.com>
2026-05-29 11:34:56 +0200

    Update course list in Bio.vue

--- views/Bio.vue
+++ views/Bio.vue
@@ -44,7 +44,11 @@
         <ul class="list-disc pl-5 mt-2 space-y-1 text-sm font-thin">
             <li>Specialization: Marketing Communication and PR</li>
             <li>Exchange semester: University of Canberra, Australia</li>
-            <li>Studied courses in digital communication, media strategy and marketing</li>
+            <li>Studied courses in:</li>
+            <li>- [Course name 1]</li>
+            <li>- [Course name 2]</li>
+            <li>- [Course name 3]</li>
+            <li>- [Course name 4]</li>
         </ul>
     </section>
 
