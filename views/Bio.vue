49a1334724e4b6fbf095e367cf249deb60be7985
Nadia Dahlberg <nadiadahlberg@outlook.com>
2026-06-01 12:27:48 +0200

    Update Languages section styling in Bio.vue

--- views/Bio.vue
+++ views/Bio.vue
@@ -16,8 +16,8 @@
     </div>
 
         <div class="pt-7">
-        <h3 class="underline font-bold text-md pb-1">Languages</h3>
-        <span class="inline-block px-3 py-2.5 text-xs border border-gray-300 rounded-full">
+        <h3 class="underline font-bold text-md pb-2.5">Languages</h3>
+        <span class="inline-block px-3 py-1 text-xs border border-gray-300 rounded-full">
   English
 </span>
             <p class="font-thin text-sm pb-2.5">
