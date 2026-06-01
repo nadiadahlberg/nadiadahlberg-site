dc52cbc81de09b658cec2910f24ab40353b5a418
Nadia Dahlberg <nadiadahlberg@outlook.com>
2026-06-01 11:42:52 +0200

    Update educational details in Bio.vue

--- views/Bio.vue
+++ views/Bio.vue
@@ -72,11 +72,11 @@
 
     <section class="pt-6">
         <h4 class="text-lg font-semibold">Bachelor’s Degree in Media and Communication Science</h4>
-        <p class="text-gray-600 text-xs">Jönköping University • 2014 – 2017</p>
+        <p class="text-gray-600 text-xs">Jönköping University, Sweden • 2014 – 2017</p>
         <ul class="list-disc pl-5 mt-2 space-y-1 text-sm font-thin">
             <p class="font-thin text-sm pb-2.5">Specialization: Marketing Communication and PR</p>
-            <p class="text-gray-600 text-xs">University of Canberra • 2016</p>
-            <p class="font-thin text-sm pb-2.5">Exchange semester: University of Canberra, Australia</p>
+            <p class="text-gray-600 text-xs">University of Canberra, Australia • 2016</p>
+            <p class="font-thin text-sm pb-2.5">Exchange semester</p>
         <p class="font-thin text-sm pb-2.5">Studied couses in:</p>
             <ul class="list-disc pl-5 mt-2 space-y-1 text-sm font-thin">
             <li>- Course name 1</li>
