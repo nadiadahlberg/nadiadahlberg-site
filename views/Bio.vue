be23465a443bc8c0df5c51bac6ce44317543b741
Nadia Dahlberg <nadiadahlberg@outlook.com>
2026-06-01 20:32:04 +0200

    Update education duration in Bio.vue

--- views/Bio.vue
+++ views/Bio.vue
@@ -194,7 +194,7 @@
     
     <section class="pt-6">
         <h4 class="text-lg font-semibold">Game Programming</h4>
-        <p class="text-gray-600 text-xs">Forsbergs Skola • 2020 – 2025</p>
+        <p class="text-gray-600 text-xs">Forsbergs Skola • 2020 – 2022</p>
         <ul class="list-disc pl-5 mt-2 space-y-1 text-sm font-thin">
             <li>Text här</li>
         </ul>
