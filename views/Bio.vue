8222c3430f43e03a9d69449c25677d646a4539f9
Nadia Dahlberg <nadiadahlberg@outlook.com>
2026-06-04 09:48:13 +0200

    Change heading from h3 to h4 for Languages section

--- views/Bio.vue
+++ views/Bio.vue
@@ -47,7 +47,9 @@
     </section>
     
 <div class="pt-7">
-    <h3 class="underline font-bold text-md pb-2.5">Languages</h3>
+        <h4 class="text-lg font-semibold">
+        Languages
+    </h4>
 
     <div class="flex flex-wrap gap-2">
         <span class="px-3 py-1 text-xs border border-gray-300 rounded-full">
